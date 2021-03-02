97f4ea8e5f9c5542c31bb4fa6667c800641aa0b9
dawwik <dawid.wiktorowski22@gmail.com>
2021-03-02 20:14:27 +0100

    added streams

--- src/com/dawid/library/io/ConsolePrinter.java
+++ src/com/dawid/library/io/ConsolePrinter.java
@@ -9,33 +9,33 @@ import java.util.Collection;
 
 public class ConsolePrinter {
     public void printBooks(Collection<Publication> publications) {
-        int counter = 0;
-        for (Publication publication : publications) {
-            if(publication instanceof Book) {
-                printLine(publication.toString());
-                counter++;
-            }
-        }
-        if (counter == 0)
+        long count = publications.stream()
+                .filter(publication -> publication instanceof Book)
+                .map(Publication::toString)
+                .peek(this::printLine)
+                .count();
+        if (count == 0) {
             printLine("Brak książek w bibliotece");
+        }
     }
 
     public void printMagazines(Collection<Publication> publications) {
-        int counter = 0;
-        for (Publication publication : publications) {
-            if(publication instanceof Magazine) {
-                printLine(publication.toString());
-                counter++;
-            }
-        }
-        if (counter == 0)
+        long count = publications.stream()
+                .filter(publication -> publication instanceof Magazine)
+                .map(Publication::toString)
+                .peek(this::printLine)
+                .count();
+        if (count == 0)
             printLine("Brak magazynów w bibliotece");
     }
 
-    public void printUsers(Collection<LibraryUser> users){
-        for(LibraryUser user : users){
+    public void printUsers(Collection<LibraryUser> users) {
+        for (LibraryUser user : users) {
             printLine(user.toString());
         }
+        users.stream()
+                .map(LibraryUser::toString)
+                .forEach(this::printLine);
     }
 
     public void printLine(String text) {
